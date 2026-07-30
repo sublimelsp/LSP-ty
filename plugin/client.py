66cb30b423f0b96359c020b779d4f535c2ddf264
skarasov <sergiy.karasov@gmail.com>
2026-07-30 19:21:26 +0300

    fix: starting dir of ty server

--- plugin/client.py
+++ plugin/client.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 from typing import Any
 
 from LSP.plugin import ClientNotification, LspPlugin, OnPreStartContext
@@ -23,7 +24,8 @@ class LspTyPlugin(LspPlugin):
         context.variables.update({
             "server_path": server_path,
         })
-
+        if not context.workspace_folders:
+            context.working_directory = os.path.dirname(context.view.file_name())
     # ----- #
     # hooks #
     # ----- #
